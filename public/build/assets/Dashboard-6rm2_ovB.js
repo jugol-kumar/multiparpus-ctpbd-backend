6045ccfad95f04891dbf936e36c84ea184a8194a
jugol-kumar <jk23717933@gmail.com>
2024-01-17 13:36:56 +0600

    change in deployment branch

---
+++ public/build/assets/Dashboard-6rm2_ovB.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s,a as c}from"./app-_aaoa6OJ.js";const t={},n=c("h1",null,"This is home components",-1),a=[n];function r(_,d){return s(),o("div",null,a)}const l=e(t,[["render",r]]);export{l as default};
